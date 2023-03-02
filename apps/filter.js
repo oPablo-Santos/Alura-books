054ca99bdfbb108685143b948958f45fbb6ed39b
oPablo-Santos <pablo_p9_santos@hotmail.com>
2023-03-02 19:47:28 -0300

    update

--- apps/filter.js
+++ apps/filter.js
@@ -6,7 +6,27 @@ function filtrarLivros() {
   const categoria = elementoBtn.value;
   let livrosFiltrados =
     categoria == "disponivel"
-      ? livros.filter((livro) => livro.quantidade > 0)
-      : livros.filter((livro) => livro.categoria == categoria);
+      ? filtrarPorDisponibilidade()
+      : FiltrarPorCategoria(categoria);
   exibirOsLivrosNaTela(livrosFiltrados);
+  if (categoria == "disponivel") {
+    const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+    exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal);
+  }
+}
+
+function FiltrarPorCategoria(categoria) {
+  return livros.filter((livro) => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter((livro) => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `;
 }
